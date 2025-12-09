0eec6ab44a13d316af9726ac1f9638c4a858fbb3
F1sh-2 <sean.rathborne6@gmail.com>
2025-12-09 11:23:24 +0000

    Ladders Update
    9/12/2025

---
+++ Assets/Scripts/Ladders.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Ladders : MonoBehaviour
+{
+    private LaddersAbility laddersAbility;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        laddersAbility= collision.GetComponent<LaddersAbility>();
+
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(laddersAbility!= null)
+        {
+            if (laddersAbility.isPermitted)
+            {
+                laddersAbility.canGoOnLadder = true;
+            }
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(laddersAbility!= null)
+        {
+            if( laddersAbility.isPermitted)
+            {
+                laddersAbility.canGoOnLadder = false;    
+            }
+        }
+    }
+}
--- Assets/Scripts/Player/LaddersAbility.cs
+++ Assets/Scripts/Player/LaddersAbility.cs
@@ -1,16 +1,78 @@
+using Unity.VisualScripting.Dependencies.Sqlite;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class LaddersAbility : MonoBehaviour
+public class LaddersAbility : BaseAbility
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public InputActionReference ladderActionRef;
+    [SerializeField] private float climbSpeed;
+    [SerializeField] private float setMinLadderTime;
+    private float minimumLadderTime;
+    private bool climb;
+    public bool canGoOnLadder;
+
+
+    protected override void Initialization()
     {
-        
+        base.Initialization();
+        minimumLadderTime = setMinLadderTime;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        ladderActionRef.action.performed += TryToClimb;
+        ladderActionRef.action.canceled += StopClimb;
+    }
+
+    private void OnDisable()
+    {
+        ladderActionRef.action.performed -= TryToClimb;
+        ladderActionRef.action.canceled -= StopClimb;
+    }
+
+    private void TryToClimb(InputAction.CallbackContext value)
+    {
+        if (!isPermitted)   
+        {
+            return;
+        }
+        if(linkedStateMachine.currentState==PlayerStates.State.Ladders || linkedStateMachine.currentState == PlayerStates.State.Dash || !canGoOnLadder)
+        {
+            return;
+        }
+
+        linkedStateMachine.ChangeState(PlayerStates.State.Ladders);
+        linkedPhysics.DisableGravity();
+        linkedPhysics.ResetVelocity();
+        climb = true;
+        minimumLadderTime = setMinLadderTime;
+    }
+    private void StopClimb(InputAction.CallbackContext value)
+    {
+        if (!isPermitted)
+        {
+            return;
+        }
+        if (linkedStateMachine.currentState != PlayerStates.State.Ladders)
+        {
+            return;
+        }
+        linkedPhysics.ResetVelocity();
+    }
+
+    public override void ProcessAbility()
+    {
+        if (climb)
+        {
+            minimumLadderTime -= Time.deltaTime;
+        }
+    }
+
+   public override void ProcessFixedAbility()
+    {
+        if (climb)
+        {
+            linkedPhysics.rb.linearVelocity = new Vector2(0, linkedInput.verticalInput * climbSpeed);
+        }
     }
 }
